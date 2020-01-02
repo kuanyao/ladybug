2521731bc20d4ee525a1a09150c370019b499978
Kuan Yao <kuanyao@amazon.com>
2020-01-02 11:48:17 -0500

    Upgrade to Okapi 4.0

--- include/main.h
+++ include/main.h
@@ -63,11 +63,13 @@
 #ifdef __cplusplus
 extern "C" {
 #endif
+
 void autonomous(void);
 void initialize(void);
 void disabled(void);
 void competition_initialize(void);
 void opcontrol(void);
+
 #ifdef __cplusplus
 }
 #endif
